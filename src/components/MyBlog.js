4d9d88c6575a23e9b183e4dec8c89b324547585a
Jagrutee Banda <jagrutee@carwale.com>
2021-04-13 19:12:22 +0530

    Added MyBlog page and css to it

--- src/components/MyBlog.js
+++ src/components/MyBlog.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import '../styles/MyBlog.css';
 
 class MyBlog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blogPosts: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			blogPosts: []
+		}
+	}
 
-  componentDidMount() {
-    const POSTS_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jagruteebanda99';
-    axios.get(POSTS_URL)
-      .then(response => {
-        if (response) {
-          this.setState({
-            blogPosts: response.data.items
-          });
-        }
-      })
-      .catch(error => console.log("Jagz posts api error: ", error));
-  }
+	componentDidMount() {
+		const POSTS_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jagruteebanda99';
+		axios.get(POSTS_URL)
+			.then(response => {
+				if (response) {
+					this.setState({
+						blogPosts: response.data.items
+					});
+				}
+			})
+			.catch(error => console.log("Jagz posts api error: ", error));
+	}
 
-  render() {
-    const { blogPosts } = this.state;
-    return (
-      <div className={"my-blog"}>
-        {
-          blogPosts.length > 0 && blogPosts.map((item, i) => <div key={`blog_post_${i}`}>
-            <p>{item.title}</p>
-            {item.description}
-          </div>)
-        }
-      </div>
-    )
-  }
+	render() {
+		const { blogPosts } = this.state;
+		return (
+			<div className="my-blog" >
+				{
+					blogPosts.length > 0 && blogPosts.map((item, i) =>
+						<div
+							key={`blog_post_${i}`}
+							className="blog-post-card" >
+							<div className="blog-post-card-header">
+								<h3 className="blog-post-card-title">{item.title}</h3>
+							</div>
+							<div className="blog-post-card-content" dangerouslySetInnerHTML={{ __html: item.description }} /> 
+							<div className="read-more-view"><a href={item.link} target="_blank" className="read-more-button">Read more</a></div>
+						</div>)
+				}
+			</div>
+		)
+	}
 }
 
 export default MyBlog;
